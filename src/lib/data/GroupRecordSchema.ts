8020a6eefbf60437fd915414ae3964b2e267ca27
zaydbkhan <zaydbkhan@gmail.com>
2025-04-26 11:03:52 -0700

    renamed groupingreport to grouprecord

--- src/lib/data/GroupRecordSchema.ts
+++ src/lib/data/GroupRecordSchema.ts
@@ -1,23 +1,23 @@
-import GroupingReport from "../dtos/GroupingReport";
+import GroupRecord from "../dtos/GroupRecord";
 import { supabase } from "../supabase";
 
-export default class GroupingReportSchema {
+export default class GroupRecordSchema {
     static async get_schema(id: number) {
         return await supabase
-            .from('GroupingReport')
+            .from('GroupRecord')
             .select()
             .eq('id', id)
     }
 
     static async get_by_assignment(assignment_id: number) {
         return await supabase
-            .from('GroupingReport')
+            .from('GroupRecord')
             .select()
             .eq('assignment_id', assignment_id)
     }
 
     static to_dto(schema: any) {
-        return new GroupingReport(
+        return new GroupRecord(
             schema.date,
             schema.alpha,
             schema.beta,
