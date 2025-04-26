35a6f02bf5b8af3e99658241980ef9e90b330f06
zaydbkhan <zaydbkhan@gmail.com>
2025-04-26 08:31:27 -0700

    continued writing grouping logic

--- src/lib/Grouper.ts
+++ src/lib/Grouper.ts
@@ -3,11 +3,36 @@ import Student from "./data/Student";
 import Group from "./data/Group";
 import GroupingReport from "./data/GroupingReport";
 
-export default class MathematicalGrouper { 
-    static create_grouping_report(assignment: Assignment, students: Student[], pairing_params: [number, number]) {
+export default class Grouper {
+    static create_grouping_report(group_size: number, assignment: Assignment, students: Student[], pairing_params: [number, number]) {
         const report = new GroupingReport(new Date().toISOString(), assignment.id, [], pairing_params);
+        const num_groups = Math.round(students.length / group_size);
+        const groups = [];
 
-        
+        let copy_students = [...students];
+
+        try {
+            for (let i = 0; i < num_groups; i++) {
+                let idx = Math.floor(Math.random()*copy_students.length);
+                groups.push([copy_students[idx]]);
+                copy_students.splice(idx, 1);
+            }
+        } catch (error) {
+            throw new Error("Not enough students for a group that size!");
+        }
+
+        while (true) {
+            for (let i = 0; i < num_groups; i++) {
+                let group = groups[i];
+                let avg_score_per_question = [];
+                let avg_personality_vec = [];
+                
+                for (let j = 0; j < assignment.max_scores.length; j++) {
+
+                }
+                
+            }
+        }
     }
 
     static mean_squared_difference(vector1: number[], vector2: number[]) {
