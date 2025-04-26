8499d298d4a275a6cdcd1c388c15b1c3b5a0c0e4
Najm Hoda <najmkhoda@gmail.com>
2025-04-25 22:08:42 -0700

    Fix import error

--- src/lib/csv.ts
+++ src/lib/csv.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import Papa from 'papaparse';
-import Student from '../../data/Student';
+import Student from '@/lib/data/Student';
 import { collection, doc, writeBatch } from 'firebase/firestore';
 import { db } from './firebase';
 
