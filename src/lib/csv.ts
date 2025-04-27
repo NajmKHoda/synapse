98193e8d8c277120eb2bdc4d4b2fc13961e8cc4e
Najm Hoda <najmkhoda@gmail.com>
2025-04-27 00:05:54 -0700

    Improve CSV parsing

--- src/lib/csv.ts
+++ src/lib/csv.ts
@@ -12,57 +12,79 @@ export default async function parseCSV(file: File, classId: string) {
         })
     });
 
-    let rows: CSVRow[];
+    // Get existing students
+    const { data: existingStudents, error: existingStudentsError } = await supabase.from('Student')
+        .select('id, email')
+        .eq('class_id', classId);
+    if (existingStudentsError) {
+        console.log('Failed to fetch existing students:', existingStudentsError);
+        return false;
+    }
+
+    let students: Student[] = [];
     try {
-        rows = CSVSchema.parse(result.data);
+        let csv = CSVSchema.parse(result.data);
+        let maxScores = csv[0].slice(2) as number[];
+        let rows: StudentRow[] = csv.slice(1);
+        students = rows.map(r => ({
+            id: existingStudents.find(e => e.email === r[1])?.id || null,
+            name: r[0],
+            email: r[1],
+            scores: r.slice(2).map((score, i) => {
+                return Math.min(1, (score as number) / maxScores[i]);
+            })
+        }));
     } catch (e) {
         throw new Error(`CSV validation failed: ${(e as any).message}`)
     }
-    
-    // Create assignment associated with CSV
-    const { data: assignments, error: assignmentError } = await supabase.from('Assignment')
-        .insert([{ name: file.name, class_id: classId, max_points: [], }])
-        .select();
 
+    // Create assignment associated with CSV
+    const { data: assignment, error: assignmentError } = await supabase.from('Assignment')
+        .insert([{ name: file.name, class_id: classId }])
+        .select().single();
     if (assignmentError) {
         console.log('Failed to create assignment:', assignmentError);
         return false;
     }
-    const assignmentId = assignments[0].id;
 
-    // Create students associated with class from CSV
-    const { data: students, error: studentError } = await supabase.from('Student')
-        .insert(rows.map(r => ({
-            name: r[0],
-            email: r[1],
+    // Create students that don't exist yet
+    const { data: studentIds, error: studentIdsError } = await supabase.from('Student')
+        .insert(students.filter(s => s.id === null).map(s => ({
+            name: s.name,
+            email: s.email,
             class_id: classId
         })))
-        .select('name, id');
-    
-    if (studentError) {
-        console.log('Failed to create students:', studentError);
+        .select('email, id');
+    if (studentIdsError) {
+        console.log('Failed to create students:', studentIdsError);
         return false;
     }
-    const studentIds = Object.fromEntries(students.map(s => [s.name, s.id]));
+
+    // Put IDs in new students
+    const studentIdMap = Object.fromEntries(studentIds.map(s => [s.email, s.id]));
+    students.forEach(s => s.id ??= studentIdMap[s.email]);
     
     // Create scores associated with students and assignment
     const { error: scoreError } = await supabase.from('Score')
-        .insert(rows.map(r => ({
-            student_id: studentIds[r[0]],
-            assignment_id: assignmentId,
-            data: r.slice(2)
+        .insert(students.map(s => ({
+            student_id: s.id,
+            assignment_id: assignment.id,
+            data: s.scores
         })));
-    
     if (scoreError) {
         console.log('Failed to create scores:', scoreError);
         return false;
     }
 
-    return true
+    return true;
 }
 
 const CSVSchema = z
-    .array(
+    .tuple([
+        z.tuple([z.literal(''), z.literal('')])
+        .rest(z.preprocess(x => Number(x), z.number())),
+    ])
+    .rest(
         z.tuple([z.string(), z.string()])
         .rest(z.preprocess(x => Number(x), z.number()))
     ).refine(rows => {
@@ -71,4 +93,12 @@ const CSVSchema = z
         return rows.every(r => r.length === len);
     }, { message: 'All rows must have the same number of columns' });
 
-type CSVRow = z.infer<typeof CSVSchema>[number];
+type CSVSchema = z.infer<typeof CSVSchema>;
+type StudentRow = CSVSchema[1];
+
+interface Student {
+    id: string | null;
+    name: string;
+    email: string;
+    scores: number[];
+}
